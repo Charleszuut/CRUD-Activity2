b265c5bbf0e816771f801faa200e1203fe6f3ee7
Charleszuut <decaynation870@gmail.com>
2025-02-05 19:41:22 +0800

    Add files via upload

--- database/database.php
+++ database/database.php
@@ -1,1 +1,20 @@
+<?php
 
+try {
+
+  $host = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "fitness_app";
+
+
+  $conn = new mysqli($host, $username, $password, $database);
+
+  if ($conn->connect_error) {
+    die("Database connection unsuccessful" . $conn->connect_error);
+    return;
+  }
+
+} catch (\Exception $e) {
+  echo "Error: " . $e;
+}
